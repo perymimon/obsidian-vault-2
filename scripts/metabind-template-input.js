77f4a0c31c2c52f35bc3530bdf0a7a72f4c01b34
pery mimon <pery.mimon@gmail.com>
2024-01-17 12:36:35 +0200

    vault backup: 2024-01-17 12:36:34
    Affected files:
    .obsidian/workspace.json
    scripts/metabind-template-input.js

--- scripts/metabind-template-input.js
+++ scripts/metabind-template-input.js
@@ -1,10 +1,14 @@
 debugger
-let lib = await engine.importJs('scripts/flat-object.js');
-let t = document.activeElement
-if(!t) throw "there is no active element"
-let frontmatter = lib.flattenObject(context.metadata.frontmatter)
-let template = t.value
-text = template.replace(/{{([^}]+?)}}/g,(_,path)=>{
+let metaData = app.plugins.plugins["obsidian-meta-bind-plugin"]
+let {flattenObject} = (await engine.importJs('scripts/flat-object.js'));
+let file = app.workspace.activeEditor.file
+let cache =  metaData.metadataManager.cache.get(file.path)
+let template = cache.memory["inputText"]
+let frontmatter = flattenObject(context.metadata.frontmatter)
+let text = template.replace(/{{([^}]+?)}}/g,(_,path)=>{
     return frontmatter[path.trim()]
 })
+//save
+let t = document.activeElement
+if(!t) throw "there is no active element"
 t.value = text;
